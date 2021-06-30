0c52a8d51604ff5b68633dcdc27a9f8c320fe03d
Samuel Thornton <79607190+samfbthornton@users.noreply.github.com>
2021-06-30 15:27:13 +0100

    Update App.java

--- src/operators/App.java
+++ src/operators/App.java
@@ -4,25 +4,15 @@ public class App {
 
 	public static void main(String[] args) {
 
-		System.out.println("Physics Result: ");
+		System.out.println("Physics Result: " + ScienceExams.physicsResult);
 
-		System.out.println(ScienceExams.physicsResult);
+		System.out.println("\nChemistry Result: " + ScienceExams.chemistryResult);
 
-		System.out.println("\nChemistry Result: ");
+		System.out.println("\nBiology Result: " + ScienceExams.biologyResult);
 
-		System.out.println(ScienceExams.chemistryResult);
+		System.out.println("\nTotal Score: " + ScienceExams.totalScore());
 
-		System.out.println("\nBiology Result: ");
-
-		System.out.println(ScienceExams.biologyResult);
-
-		System.out.println("\nTotal Score: ");
-
-		System.out.println(ScienceExams.totalScore());
-
-		System.out.println("\nPercentage: ");
-
-		System.out.println(ScienceExams.getPercentage());
+		System.out.println("\nPercentage: " + ScienceExams.getPercentage());
 
 	}
 
